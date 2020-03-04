4c3e7c802079fef56702d81f79e930fa19eaac75
rkwitt <rkwitt@gmx.at>
2020-03-04 09:45:51 +0100

    Added run_experiments* scripts

--- run_experiments_jacobian.py
+++ run_experiments_jacobian.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     mp.set_start_method('spawn', force=True)
 
-    output_root_dir = '/scratch1/chofer/cifar10/vanilla_1k'
+    output_root_dir = '/scratch2/chofer/cifar10_vanilla_w_jacobian'
 
     config_topo = {
         "output_root_dir":         [output_root_dir],
@@ -15,15 +15,13 @@ if __name__ == '__main__':
         "ds_train_name":           ["cifar10_train"],
         "ds_test_name":            ["cifar10_test"],
         "ds_normalization":        [True],
-        "num_train_samples":       [1000],
+        "num_train_samples":       [500],
         "num_augmentations":       [1],
         "model_name":              [
-             #("SimpleCNN_MNIST", {'batch_norm': True,
-             #                    'cls_spectral_norm': False}),        
-             ("SimpleCNN13", {'batch_norm':        True,
-                              'cls_spectral_norm': False,
-                              'drop_out':          True,
-                              'final_bn':          True}),
+            ("SimpleCNN13", {'batch_norm': True,
+                             'cls_spectral_norm': False,
+                             'drop_out': True,
+                             'final_bn': True}),
         ],
         "type_augmentation":        ["standard"],
         "batch_size":              [32],
@@ -31,24 +29,24 @@ if __name__ == '__main__':
             "CrossEntropyLoss",
         ],
         "num_epochs":              [310*4],
-        "lr_init":                 [0.1,0.3,0.5],
+        "lr_init":                 [0.1, 0.3, 0.5],
         "num_intra_samples":       [1],
-        "w_top_loss":              [0.0],
-        "top_scale":               [0.0], #[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9],
+        "w_top_loss":              [0.],
+        "top_scale":               [0.],
         "weight_decay_feat_ext":   [0.001],
-        "weight_decay_cls":        [0.0001, 0.0005, 0.001],
-        "normalize_gradient":      [False], 
+        "weight_decay_cls":        [0.0005,0.0001],
+        "lambda_JR":               [1e-3,1e-4],
+        "normalize_gradient":      [False],
         "pers_type":               ["VrPersistenceL_2"],
-        "tag":                     ['vanilla_1k'],
+        "tag":                     ['CIFAR10_jacobian'],
         "compute_persistence":     [False],
         "track_model":             [False]
     }
-    
- 
+
     config = configs_from_grid(config_topo)
 
     mp = True
 
     if mp:
         config = [((), c) for c in config]
-        scatter_fn_on_devices(experiment_blue_print, config, [0,1,2], 3)
+        scatter_fn_on_devices(experiment_jacobian, config, [0,1], 3)
--- run_experiments_statreg.py
+++ run_experiments_statreg.py
@@ -1,25 +1,25 @@
 if __name__ == '__main__':
     from pathlib import Path
     from chofer_torchex.utils.run_experiments import configs_from_grid, scatter_fn_on_devices
-    from core.experiments import experiment_blue_print, experiment_jacobian
+    from core.experiments import experiment_blue_print, experiment_jacobian, experiment_statreg
     import torch.multiprocessing as mp
     # import torch
 
     mp.set_start_method('spawn', force=True)
 
-    output_root_dir = '/scratch1/chofer/cifar10/vanilla_1k'
+    output_root_dir = '/scratch2/chofer/cifar10_statreg_VR'
 
-    config_topo = {
+    config = {
         "output_root_dir":         [output_root_dir],
         "cv_run_num":              [10],
         "ds_train_name":           ["cifar10_train"],
         "ds_test_name":            ["cifar10_test"],
         "ds_normalization":        [True],
-        "num_train_samples":       [1000],
+        "num_train_samples":       [500],
         "num_augmentations":       [1],
         "model_name":              [
              #("SimpleCNN_MNIST", {'batch_norm': True,
-             #                    'cls_spectral_norm': False}),        
+             #                     'cls_spectral_norm': False}),   
              ("SimpleCNN13", {'batch_norm':        True,
                               'cls_spectral_norm': False,
                               'drop_out':          True,
@@ -30,25 +30,22 @@ if __name__ == '__main__':
         "cls_loss_fn":             [
             "CrossEntropyLoss",
         ],
+        "statreg_loss_fn":         ['VR_loss'],
+        "w_statreg_loss":          [0.0001, 0.001, 0.01, 0.1],             
         "num_epochs":              [310*4],
         "lr_init":                 [0.1,0.3,0.5],
         "num_intra_samples":       [1],
-        "w_top_loss":              [0.0],
-        "top_scale":               [0.0], #[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9],
         "weight_decay_feat_ext":   [0.001],
         "weight_decay_cls":        [0.0001, 0.0005, 0.001],
-        "normalize_gradient":      [False], 
-        "pers_type":               ["VrPersistenceL_2"],
-        "tag":                     ['vanilla_1k'],
-        "compute_persistence":     [False],
+        "tag":                     ['cifar10_statreg_VR'],
         "track_model":             [False]
     }
     
  
-    config = configs_from_grid(config_topo)
+    config = configs_from_grid(config)
 
     mp = True
 
     if mp:
         config = [((), c) for c in config]
-        scatter_fn_on_devices(experiment_blue_print, config, [0,1,2], 3)
+        scatter_fn_on_devices(experiment_statreg, config, [2,3], 2)
